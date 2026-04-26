08b6100a5c89c0336dd2a3267d5258dc95621977
Catalysxt <ahmadsarraj99@outlook.com>
2026-04-26 21:59:24 +1000

    Refactored to use std::ranges::find_if() to search for first sig fig

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cctype>
 #include <format>
 #include <string_view>
+#include <algorithm> // For std::ranges::find_if
 
 //******************************************************************************
 // Decimal to Scientific Notation Calculator
@@ -46,29 +47,23 @@ std::string to_scientific(std::string_view input) {
     // Step 1: Detect if input is negative
     bool negative = is_input_negative(input);
 
-    int exponent {};
-    int first_sig_fig {};
     int decimal_pt {};
-
+    
     decimal_pt = input.find(".");
-
+    
     // If no decimal pt detected, assign it the length of the number
     if(decimal_pt == std::string_view::npos) {
         decimal_pt = static_cast<int>(input.size()); 
     }
-
+    
     // Step 3: Locate first sig fig
-    bool found_sig_fig = false;
-    for (std::size_t i = 0; i < input.size(); ++i) {
-        if (input[i] != '.' && input[i] != '0' && std::isdigit(static_cast<unsigned char>(input[i]))) {
-            first_sig_fig = static_cast<int>(i);
-            found_sig_fig = true;
-            break;
-        }
-    }
+    auto it = std::ranges::find_if(input, [](unsigned char c) { return std::isdigit(c) && c != '0' && c != '.'; } );
+    
+    int first_sig_fig {};
+    first_sig_fig = std::ranges::distance(input.begin(), it);
 
-    if(!found_sig_fig) {      // The input doesn't contain a nonzero number
-        return std::string(negative ? "-" : "") + "0e0";
+    if (it == input.end()) {      // The input doesn't contain a nonzero number
+        return std::string(negative ? "-0e0" : "0e0");
     }
 
     // Step 4: Calculate left/right shift
@@ -76,6 +71,7 @@ std::string to_scientific(std::string_view input) {
     // At the end we want first_sig_index == decimal_index + 1
 
     // left shift, increment exponent
+    int exponent {};
     if (decimal_pt > first_sig_fig) {
         exponent = decimal_pt - first_sig_fig - 1;
     }
